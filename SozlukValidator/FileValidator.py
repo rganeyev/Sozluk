cb93c0ae0979c6fb435620cb24ec3869aaadf5c4
Rustam Ganeyev <rganeyev@gmail.com>
2013-07-23 14:22:48 +0400

    added file validator (now it only removes duplicates)

--- SozlukValidator/FileValidator.py
+++ SozlukValidator/FileValidator.py
@@ -1,19 +1,31 @@
-__author__ = 'rganeyev'
+__author__ = 'Rustam Ganeyev'
 
 def removeCopies(list):
     n = len(list)
-    result = [];
-    for i in range(0, n - 1):
-        if list[i] != list[i + 1]
+    result = []
+    lastWord = ""
+    for word in list:
+        if word != lastWord:
+            result.append(word)
+        lastWord = word
+
+    return result
 
 
 def main():
-    letters = ["a", "b"];
+    letters = ["a", "b"]
     for letter in letters:
         fileName = letter + ".txt"
         f = open(fileName)
         list = f.readlines()
+        f.close()
+        #operations
+        list = removeCopies(list)
 
-        removeCopies(list)
+
+        #writing to file
+        f = open(fileName, "w")
+        f.write("\n".join(list))
+        f.close()
 
 main()
